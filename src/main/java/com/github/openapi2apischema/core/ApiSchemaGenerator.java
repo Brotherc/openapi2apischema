cd3f1d47d18de3d2ecbbc659e3460127773c4961
liuchunsheng <1157730239@qq.com>
2025-05-21 21:15:48 +0800

    refactor: 代码优化

--- src/main/java/com/github/openapi2apischema/core/ApiSchemaGenerator.java
+++ src/main/java/com/github/openapi2apischema/core/ApiSchemaGenerator.java
@@ -3,7 +3,7 @@ package com.github.openapi2apischema.core;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.openapi2apischema.core.enums.OpenApiVersion;
 import com.github.openapi2apischema.core.model.*;
-import io.swagger.models.*;
+import com.github.openapi2apischema.core.parse.ApiParser;
 import io.swagger.models.auth.AuthorizationValue;
 import io.swagger.parser.Swagger20Parser;
 
@@ -20,20 +20,16 @@ public class ApiSchemaGenerator {
     public static List<ApiSchema> generateBySwaggerUrl(
             OpenApiVersion openApiVersion, String swaggerUrl, List<AuthorizationValue> auths) throws IOException {
         if (OpenApiVersion.V2.equals(openApiVersion)) {
-            return parse20(new Swagger20Parser().read(swaggerUrl, auths));
+            return ApiParser.parse(new Swagger20Parser().read(swaggerUrl, auths));
         }
         return Collections.emptyList();
     }
 
     public static List<ApiSchema> generateBySwaggerJson(OpenApiVersion openApiVersion, String swaggerJson) throws IOException {
         if (OpenApiVersion.V2.equals(openApiVersion)) {
-            return parse20(new Swagger20Parser().parse(swaggerJson));
+            return ApiParser.parse(new Swagger20Parser().parse(swaggerJson));
         }
         return Collections.emptyList();
-    }
-
-    private static List<ApiSchema> parse20(Swagger read) {
-        return null;
     }
 
 }

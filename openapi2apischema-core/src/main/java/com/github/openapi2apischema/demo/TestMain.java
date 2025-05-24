2d733ecc3ba5a4e2508046e3aec8786a120be9f3
liuchunsheng <1157730239@qq.com>
2025-05-24 10:51:35 +0800

    refactor: 修改测试方法

--- openapi2apischema-core/src/main/java/com/github/openapi2apischema/demo/TestMain.java
+++ openapi2apischema-core/src/main/java/com/github/openapi2apischema/demo/TestMain.java
@@ -13,12 +13,20 @@ public class TestMain {
 
     // 替换为实际的swagger接口地址
     private static final List<String> swaggerUrl = Arrays.asList(
-            "https://pre-mobile.utyun.com/auth-enterprise/v2/api-docs"
+            "http://xxx/v2/api-docs"
+    );
+
+    private static final List<String> swaggerUrlV3 = Arrays.asList(
+            "http://xxx/v3/api-docs"
     );
 
     public static void main(String[] args) throws IOException {
         for (String url : swaggerUrl) {
             List<ApiSchema> apiSchemas = ApiSchemaGenerator.generateBySwaggerUrl(OpenApiVersion.V2, url, null);
+            System.out.println(new ObjectMapper().writeValueAsString(apiSchemas));
+        }
+        for (String url : swaggerUrlV3) {
+            List<ApiSchema> apiSchemas = ApiSchemaGenerator.generateBySwaggerUrl(OpenApiVersion.V3, url, null);
             System.out.println(new ObjectMapper().writeValueAsString(apiSchemas));
         }
     }
